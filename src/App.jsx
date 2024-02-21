48526fd8ea8a4ab5194b385b3f89eeca805624d1
victorpb96 <120343714+victorpb96@users.noreply.github.com>
2024-02-21 15:59:53 +0800

    White page - navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 
 const App = () => {
+
+  const current_theme = localStorage.getItem('current_theme');
+  const [theme, setTheme] = useState(current_theme? current_theme : 'light');
+
+  useEffect(()=>{
+    localStorage.setItem('current_theme', theme);
+  },[theme])
+
+
   return (
-    <div>
-      <Navbar/>
+    <div className={'container ${theme}'}>
+      <Navbar theme={theme} setTheme={setTheme}/>
     </div>
   )
 }
